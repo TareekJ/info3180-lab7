fc28b6f37afe0a14c93837577da69ffa7efd7b94
Tareek Johnson <tareek.johnson@mymona.uwi.edu>
2019-03-29 21:26:15 +0000

    First commit

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,11 @@
+from flask_wtf.file import FileAllowed, FileRequired, FileField
+from flask_wtf import FlaskForm
+from wtforms.fields import TextAreaField
+from wtforms.validators import DataRequired
+from wtforms import SpringField
+
+
+class UploadForm(FlaskForm):
+      description = TextAreaField('Description', validators=[DataRequired()])
+      photo = FileField('Upload Image', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'], 'Only jpg, png and jpeg is accepted')])
 
